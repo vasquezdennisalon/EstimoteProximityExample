cb5403cfd9c1be1c34ec4c47094705cb4c8e66c1
vasquezdennisalon <vasquezdennisan@gmail.com>
2018-08-14 14:12:42 -0600

    Initial Commit

---
+++ EstimoteProximity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EstimoteProximity
+//
+//  Created by Denis Vásquez on 8/14/18.
+//  Copyright © 2018 Denis Vásquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
